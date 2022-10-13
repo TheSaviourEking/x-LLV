1b086269d7f1a821fa8ac80644c8eaf66fcf24be
Saviour <saviour.eking@outlook.com>
2022-10-13 07:58:28 -0700

    add 5-printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -> printf the parenthesized text
+ * Description: a function to print the parenthesized text
+ * Return: The parenthersized text
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
