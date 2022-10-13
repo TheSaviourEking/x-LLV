d3726c6084599ec603503931180753d36daf49aa
StallionXO <eking.saviour2002@gmail.com>
2022-10-13 07:43:15 -0700

    Initial Commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -> prints out the parenthesized text
+ * Description:  use puts to print text
+ * Return: The parenthesized Characters
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
